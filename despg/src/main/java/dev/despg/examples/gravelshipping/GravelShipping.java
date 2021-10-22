47633d695b594979e4f934e4df6cfd310a8200e5
aliri <a.irion@gmx.de>
2021-10-22 13:47:52 +0200

    add license headers

--- despg/src/main/java/dev/despg/examples/gravelshipping/GravelShipping.java
+++ despg/src/main/java/dev/despg/examples/gravelshipping/GravelShipping.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2021 yamm.dev, Ralf Buschermöhle
+ * 	
+ * DESPG is made available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * see LICENSE
+ * 
+ */
 package dev.despg.examples.gravelshipping;
 
 import java.util.logging.Level;
